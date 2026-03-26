9167466cebeeddfc09a09e47186cefbbfa3c979d
hirmy630-droid <hirmy630@gmail.com>
2026-03-26 20:49:47 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,42 +1,72 @@
-const CACHE_NAME = 'tightframe-android-fullscreen-v1';
-const APP_SHELL = [
+const CACHE_VERSION = 'tightframe-v20260326-1';
+const STATIC_CACHE = `${CACHE_VERSION}-static`;
+const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
+
+const STATIC_ASSETS = [
   './',
-  './index.html',
   './manifest.json',
   './icon-180.png',
   './icon-192.png',
-  './icon-512.png',
-  './icon-1024.png'
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())
+    caches.open(STATIC_CACHE)
+      .then((cache) => cache.addAll(STATIC_ASSETS))
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(
-      keys.map((key) => key !== CACHE_NAME ? caches.delete(key) : Promise.resolve())
-    )).then(() => self.clients.claim())
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== STATIC_CACHE && key !== RUNTIME_CACHE) {
+            return caches.delete(key);
+          }
+          return Promise.resolve();
+        })
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
+
   const url = new URL(event.request.url);
 
   if (event.request.mode === 'navigate') {
     event.respondWith(
-      fetch(event.request).catch(() => caches.match('./index.html'))
+      fetch(event.request)
+        .then((response) => {
+          const responseClone = response.clone();
+          caches.open(RUNTIME_CACHE).then((cache) => cache.put('./index.html', responseClone));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
     );
     return;
   }
 
-  if (url.origin === location.origin) {
+  if (url.origin !== self.location.origin) return;
+
+  if (STATIC_ASSETS.some((asset) => url.pathname.endsWith(asset.replace('./', '/')))) {
     event.respondWith(
       caches.match(event.request).then((cached) => cached || fetch(event.request))
     );
+    return;
   }
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches.open(RUNTIME_CACHE).then((cache) => cache.put(event.request, responseClone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
